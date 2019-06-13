d21d8544991d365deba5e80f43347ec8ec0d4db5
Remy Manso <remzilla@Remys-MacBook-Pro.local>
2019-06-13 15:18:13 +0100

    added functionality to have players submit their names, displays players

--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
-get '/' do
-  'Testing infrastructure working!'
-end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/names' do
+    @player_1 = params["player_name_1"]
+    @player_2 = params["player_name_2"]
+    erb :play
+  end
 
 run! if app_file == $0
 end
--- spec/features/battle_spec.rb
+++ spec/features/battle_spec.rb
@@ -1,6 +1,15 @@
-feature 'user vists home page' do
-  scenario 'displays some text' do
-    visit('/')
-    expect(page).to have_content('Testing infrastructure working!')
+
+
+# As two Players,
+# So we can play a personalised game of Battle,
+# We want to Start a fight by entering our names and seeing them
+
+feature 'User can enter their name' do
+  scenario 'Name is displayed' do
+  visit '/'
+  fill_in('Name_1', with: 'Godzilla')
+  fill_in('Name_2', with: 'Godzuki')
+  click_button('Submit')
+  expect(page).to have_text("Godzilla vs Godzuki")
   end
 end
